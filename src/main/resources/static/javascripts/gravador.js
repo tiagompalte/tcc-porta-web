0811e07e694a79c57b7099c4c2ec333a4b624957
Tiago Mariani Palte <tiagompalte@hotmail.com>
2017-10-23 13:43:44 -0200

    Gravador P5

--- src/main/resources/static/javascripts/gravador.js
+++ src/main/resources/static/javascripts/gravador.js
@@ -1,27 +1,43 @@
-var Brewer = Brewer || {};
 
-Brewer.UploadAudio = (function() {
-		
-	function UploadAudio() {
-						
-	}
-	
-	UploadAudio.prototype.iniciar = function () {
-		
-	}
-	
-		
-	function adicionarCsrfToken(xhr) {
-		var token = $('input[name=_csrf]').val();
-		var header = $('input[name=_csrf_header').val();
-		xhr.setRequestHeader(header, token);
-	}
-	
-	return UploadAudio;
-	
-})();
+var timer = new Timer();
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#time-display').html(timer.getTimeValues().toString(['seconds', 'secondTenths']));
+}); 
 
-$(function() {
-	var uploadAudio = new Brewer.UploadAudio();
-	uploadAudio.iniciar();
+timer.addEventListener('targetAchieved', function (e) {
+    $('#time-display').html('03:00');
 });
+
+function startRecording() {
+	mic = new p5.AudioIn();
+	mic.start();
+	recorder = new p5.SoundRecorder();
+	recorder.setInput(mic);
+	soundFile = new p5.SoundFile();
+	timer.start({precision: 'secondTenths', countdown: true, startValues: {seconds: 3}});
+	recorder.record(soundFile);
+	setTimeout(stopRecording, 3000);
+	document.getElementById('record').setAttribute("disabled","disabled");
+	document.getElementById('cancel').removeAttribute("disabled");	
+}
+
+function stopRecording() {
+	timer.stop();
+	recorder.stop();
+	playRecord();
+	document.getElementById('play').removeAttribute("disabled");
+	document.getElementById('record').removeAttribute("disabled");
+	document.getElementById('cancel').setAttribute("disabled","disabled");
+}
+
+function playRecord() {
+	soundFile.play();
+}
+
+function adicionarCsrfToken(xhr) {
+	var token = $('input[name=_csrf]').val();
+	var header = $('input[name=_csrf_header').val();
+	xhr.setRequestHeader(header, token);
+}
+
+//document.getElementsByClassName('js-container-audio')["0"].attributes[1].value
