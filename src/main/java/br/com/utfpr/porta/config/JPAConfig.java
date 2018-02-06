50b999671c1faad433d10a3df85fc967ab3080b0
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-02-06 19:31:57 -0200

    Implementação de acesso ao banco de dados local ou produção

--- src/main/java/br/com/utfpr/porta/config/JPAConfig.java
+++ src/main/java/br/com/utfpr/porta/config/JPAConfig.java
@@ -1,12 +1,12 @@
 package br.com.utfpr.porta.config;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -40,29 +40,17 @@ public class JPAConfig {
 		
 	@Profile("prod")
 	@Bean
-	public DataSource dataSourceProd() throws Exception {
+	public DataSource dataSourceProd() throws URISyntaxException {
 		
-		String ambiente = System.getenv("AMBIENTE_PROD");		
-		String username, password, jdbcUrl;
-		
-		if(Strings.isEmpty(ambiente) && "AMAZON".equalsIgnoreCase(ambiente)) {			
-			String dbName = System.getProperty("RDS_DB_NAME");
-			username = System.getProperty("RDS_USERNAME");
-			password = System.getProperty("RDS_PASSWORD");
-			String hostname = System.getProperty("RDS_HOSTNAME");
-			String port = System.getProperty("RDS_PORT");
-			jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + username + "&password=" + password;			
-		}
-		else {
-			URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
-		    username = jdbUri.getUserInfo().split(":")[0];
-		    password = jdbUri.getUserInfo().split(":")[1];
-		    String port = String.valueOf(jdbUri.getPort());
-		    jdbcUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-		}
-		
+		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+
+	    String username = jdbUri.getUserInfo().split(":")[0];
+	    String password = jdbUri.getUserInfo().split(":")[1];
+	    String port = String.valueOf(jdbUri.getPort());
+	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+	    
 	    BasicDataSource dataSource = new BasicDataSource();
-	    dataSource.setUrl(jdbcUrl);
+	    dataSource.setUrl(jdbUrl);
 	    dataSource.setUsername(username);
 	    dataSource.setPassword(password);
 	    dataSource.setInitialSize(5);
