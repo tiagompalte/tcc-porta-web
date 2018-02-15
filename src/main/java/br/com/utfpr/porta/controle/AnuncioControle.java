63808df15f6555a53a75533f5733aa37309057db
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-02-15 19:49:12 -0200

    Implementar controle páginas web anúncio

---
+++ src/main/java/br/com/utfpr/porta/controle/AnuncioControle.java
@@ -0,0 +1,10 @@
+package br.com.utfpr.porta.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/anuncios")
+public class AnuncioControle {
+
+}
