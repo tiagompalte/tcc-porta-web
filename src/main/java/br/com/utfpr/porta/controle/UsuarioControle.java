8b1fa5f4f4770bcb3bcda3482a317bdb07b9fffd
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-05-05 15:11:18 -0300

    Implementar ordem no resultado das consultas do banco de dados

--- src/main/java/br/com/utfpr/porta/controle/UsuarioControle.java
+++ src/main/java/br/com/utfpr/porta/controle/UsuarioControle.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -119,8 +120,8 @@ public class UsuarioControle {
 	}
 	
 	@GetMapping
-	public ModelAndView pesquisar(UsuarioFiltro usuarioFiltro, @PageableDefault(size = 5) Pageable pageable, 
-			HttpServletRequest httpServletRequest) {
+	public ModelAndView pesquisar(UsuarioFiltro usuarioFiltro, HttpServletRequest httpServletRequest,
+			@PageableDefault(size = 5, direction = Direction.ASC, sort = "p.nome") Pageable pageable) {
 		
 		ModelAndView mv = new ModelAndView("/usuario/PesquisaUsuarios");		
 		PageWrapper<Usuario> paginaWrapper = new PageWrapper<>(
